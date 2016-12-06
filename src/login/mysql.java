2fb35511507d321d1179e4c172592f09dfd28ec9
Tim Vlaar <tim-vlaar@hotmail.com>
2016-12-06 09:05:14 +0100

    mysql aangepast

--- src/login/mysql.java
+++ src/login/mysql.java
@@ -6,13 +6,10 @@ public class Mysql {
     private final String PASSWORD = "Pulsar11";
     private final String IPADDRESS = "localhost";
     private final String POORT = "3306";
-    private final String DATABASENAAM = "wsdatabase";
+    private final String DATABASENAAM = "corendonbagagesystem";
     private final boolean AUTORECONNECT = true;
     private final boolean SSL = false;
-    
-
-    
-    //nieuwe versie
+  
     String getUsername(){
         return USENAME;
     };
